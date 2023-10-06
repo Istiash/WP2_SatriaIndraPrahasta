69c49add9c557e64922821d6dc844d670c5a1e2a
Istiash <sipcuty@gmail.com>
2023-10-06 15:32:12 +0700

    Memasukan file latihan pertemuan 3

---
+++ views/view_latihan1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+ Halo Kawan.. Yuk kita belajar web programming..!!!<br>
+ <p>Nilai 1 = <?= $nilai1; ?></p>
+ <p>Nilai 2 = <?= $nilai2; ?></p>
+ ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
